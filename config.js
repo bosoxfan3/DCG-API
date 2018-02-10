decf888fee19754ae21e4ae18bd6131aa710493e
Daniel Acquesta <danielacquesta@sbcglobal.net>
2018-02-09 22:12:01 -0800

    added test database to config file

--- config.js
+++ config.js
@@ -3,6 +3,11 @@ process.env.DATABASE_URL ||
 global.DATABASE_URL ||
 'mongodb://localhost/dcg';
 
+exports.TEST_DATABASE_URL =
+process.env.TEST_DATABASE_URL ||
+global.TEST_DATABASE_URL ||
+'mongodb://localhost/test-dcg';
+
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'donottellanyone';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
