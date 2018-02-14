ead5b654450ef4637b88bf80db2e8ae6cca741cb
Daniel Acquesta <danielacquesta@sbcglobal.net>
2018-02-14 13:08:15 -0800

    updating picks and saving them correctly

--- users/models.js
+++ users/models.js
@@ -6,14 +6,16 @@ mongoose.Promise = global.Promise;
 const UserSchema = mongoose.Schema({
   username: {type: String, required: true, unique: true},
   password: {type: String, required: true},
-  name: {type: String, required: true}
+  name: {type: String, required: true},
+  picks: {type: Object}
 });
 
 UserSchema.methods.apiRepr = function() {
   return {
     id: this._id,
     username: this.username || '',
-    name: this.name || ''
+    name: this.name || '',
+    picks: this.picks || {}
   };
 };
 
@@ -23,6 +25,20 @@ UserSchema.methods.validatePassword = function(password) {
 
 UserSchema.statics.hashPassword = function(password) {
   return bcrypt.hash(password, 10);
+};
+
+UserSchema.statics.getUserByUsername = function(username) {
+  return this.findOne({
+    username
+  })
+    .exec()
+    .then((user) => {
+      if (user) {
+        return Promise.resolve(user);
+      }
+      const err = new Error('user does not exist');
+      return Promise.reject(err);
+    });
 };
 
 const User = mongoose.model('User', UserSchema);
